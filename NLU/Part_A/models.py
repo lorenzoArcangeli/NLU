04c1d801f270d8349c955c5c30b8fa7cf150d0f8
lorenzoArcangeli <lorenz.arcangeli@studenti.unicam.it>
2025-04-08 16:28:57 +0200

    NLU_part1

--- NLU/Part_A/models.py
+++ NLU/Part_A/models.py
@@ -1,9 +1,13 @@
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
+# Vanilla
 class ModelIAS(nn.Module):
 
-    def __init__(self, hid_size, out_slot, out_int, emb_size, vocab_len, n_layer=1, pad_index=0):
+    def __init__(self, hid_size, out_slot, out_int, emb_size, vocab_len, n_layer=1, pad_index=0, bidirectional=bidirectional_mode, dropout_mode=False, dropout=0.5):
+        super(ModelIAS, self).__init__()
+        # hid_size = Hidden size
+        # out_slot = number of slots (output size for slot filling)
         super(ModelIAS, self).__init__()
         # hid_size = Hidden size
         # out_slot = number of slots (output size for slot filling)
@@ -11,12 +15,16 @@ class ModelIAS(nn.Module):
         # emb_size = word embedding size
         
         self.embedding = nn.Embedding(vocab_len, emb_size, padding_idx=pad_index)
-        
-        self.utt_encoder = nn.LSTM(emb_size, hid_size, n_layer, bidirectional=False, batch_first=True)    
+        self.utt_encoder = nn.LSTM(emb_size, hid_size, n_layer, bidirectional=bidirectional_mode, batch_first=True)    
+        self.dropout_mode = dropout_mode
+        if self.dropout_mode:
+            self.dropout = nn.Dropout(0.1) # → (possibly after the LSTM output or before the linear layers) to prevent overfitting
+        if bidirectional:
+            hid_size=hid_size*2
         self.slot_out = nn.Linear(hid_size, out_slot)
         self.intent_out = nn.Linear(hid_size, out_int)
-        # Dropout layer How/Where do we apply it?
-        self.dropout = nn.Dropout(0.1) # → (possibly after the LSTM output or before the linear layers) to prevent overfitting
+
+
         
     def forward(self, utterance, seq_lengths):
         # utterance.size() = batch_size X seq_len
@@ -27,14 +35,19 @@ class ModelIAS(nn.Module):
         packed_input = pack_padded_sequence(utt_emb, seq_lengths.cpu().numpy(), batch_first=True)
         # Process the batch
         packed_output, (last_hidden, cell) = self.utt_encoder(packed_input) 
-       
+
         # Unpack the sequence
         utt_encoded, input_sizes = pad_packed_sequence(packed_output, batch_first=True)
+
+        # Apply dropout
+        if self.dropout_mode:
+            utt_encoded = self.dropout(utt_encoded)
+
         # Get the last hidden state
-        last_hidden = last_hidden[-1,:,:]
-        
-        # Is this another possible way to get the last hiddent state? (Why?)
-        # utt_encoded.permute(1,0,2)[-1]
+        if self.utt_encoder.bidirectional:
+            last_hidden = torch.cat((last_hidden[0], last_hidden[1]), dim=1)
+        else:
+            last_hidden = last_hidden[-1,:,:]
         
         # Compute slot logits
         slots = self.slot_out(utt_encoded)
@@ -45,3 +58,4 @@ class ModelIAS(nn.Module):
         slots = slots.permute(0,2,1) # We need this for computing the loss
         # Slot size: batch_size, classes, seq_len
         return slots, intent
+
